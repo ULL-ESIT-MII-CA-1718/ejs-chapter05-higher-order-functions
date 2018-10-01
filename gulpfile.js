cdc094e318424065436a95079ba060af578c16f7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-10-01 13:46:06 +0100

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,9 @@ var shell = require('gulp-shell');
 gulp.task("default", ["test"]);
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
+
+gulp.task("docs", shell.task("asciidoctor docs/summary.adoc -o docs/index.html"));
+
+gulp.task("pre-install", shell.task([
+  "sudo gem install asciidoctor"
+]));
